97a33dc77a0fc0aa04944e71dc122fd9448ed70d
Jessie <974889924@qq.com>
2023-03-10 21:49:39 +0800

    feat: initialized the project

---
+++ src/main/java/com/example/windserver/WindServerApplication.java
@@ -0,0 +1,16 @@
+package com.example.windserver;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class WindServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WindServerApplication.class, args);
+        log.info("启动成功");
+    }
+
+}
---
+++ src/test/java/com/example/windserver/WindServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.windserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
